25afae8840640837afe7981d9eaafb86ac2ced7b
Aleister Coinley <coinyon@uberhax0r.de>
2021-02-07 20:06:26 +0100

    Add publishing of results to datapane

--- model.py
+++ model.py
@@ -1,9 +1,16 @@
 import pandas as pd
 import yfinance as yf
+import os
+
 from rotkehlchen.assets.asset import Asset
 from rotkehlchen.externalapis.coingecko import Coingecko
 
 gc = Coingecko()
+rows = []
+
+holdings = pd.read_csv("./holdings.csv", sep=';', parse_dates=['Date'])
+current_holdings = holdings.iloc[-1]
+
 
 for ETHC_ticker in ('DTSRF', 'ETHC.NE', '2KV.MU'):
     ticker = yf.Ticker(ETHC_ticker)
@@ -11,9 +18,6 @@ for ETHC_ticker in ('DTSRF', 'ETHC.NE', '2KV.MU'):
     currency = Asset(ticker.info['currency'])
     eth_price = float(gc.simple_price(Asset('ETH'), currency))
     mkr_price = float(gc.simple_price(Asset('MKR'), currency))
-
-    holdings = pd.read_csv("./holdings.csv", sep=';', parse_dates=['Date'])
-    current_holdings = holdings.iloc[-1]
 
     shares_outstanding = ticker.info['sharesOutstanding']
     price = ticker.history(period='5d').iloc[-1].Close
@@ -33,3 +37,34 @@ for ETHC_ticker in ('DTSRF', 'ETHC.NE', '2KV.MU'):
     print("NAV per share:\t{0:.2f} {1}".format(nav_per_share, currency.symbol))
     print("Share price:\t{0:.2f} {1}".format(price, currency.symbol))
     print("Premium:\t{0:+.2f} %".format((price / nav_per_share - 1) * 100, currency.symbol))
+
+    rows += [{
+        'ticker': ETHC_ticker,
+        'currency': currency.symbol,
+        'nav': nav,
+        'nav_per_share': nav_per_share,
+        'share_price': price,
+        'premium': (price / nav_per_share - 1) * 100
+    }]
+
+dp_token = os.environ.get('DATAPANE_TOKEN')
+
+if dp_token:
+    import datapane as dp
+    dp.login(token=dp_token)
+
+    df = pd.DataFrame(rows)
+    r = dp.Report(
+        f'### Holdings',
+        dp.Table(pd.DataFrame(current_holdings).iloc[1:]),
+        f'### Share Price Premium',
+        dp.Table(df),
+        f'The average premium is {df["premium"].mean():+.1f} %'
+    )
+
+    r.publish(
+        name=f'Ether Capital Corp. Premium',
+        open=False,
+        description=f'Calculate the premium of the ETHC share price compared to the holdings'
+    )
+
